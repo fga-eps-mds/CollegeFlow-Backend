6e3bba358d210649b43df50d7d355874d509b8a6
Rapha <rapha.ela12@hotmail.com>
2022-09-01 19:43:42 -0300

    feat: add review controller and routes

--- config/controllers/subjects.js
+++ config/controllers/subjects.js
@@ -22,3 +22,32 @@ exports.detailSubject = async (req, res) => {
     res.status(500).send(error.message);
   }
 };
+
+exports.createReview = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const { title, professor, rating, comment } = req.body;
+
+    const subject = await SubjectModel.findById(id);
+
+    const review = {
+      professor,
+      title,
+      comment,
+      rating,
+    };
+
+    subject.reviews.push(review);
+
+    subject.numReviews = subject.reviews.length;
+
+    subject.rating =
+      subject.reviews.reduce((acc, item) => item.rating + acc, 0) /
+      subject.reviews.length;
+
+    await subject.save();
+    res.status(200).json(review);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
--- config/routes/subject.js
+++ config/routes/subject.js
@@ -2,11 +2,13 @@ const express = require("express");
 const {
   listSubject,
   detailSubject,
+  createReview,
 } = require("../controllers/subjects");
 const router = express.Router();
 const SubjectModel = require("../models/subject");
 
 router.get("/subject", listSubject);
 router.get("/subject/:id", detailSubject);
+router.post("/subject/:id/review", createReview);
 
 module.exports = router;
